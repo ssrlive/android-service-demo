0cf74eefc325279f7bdef639a3f3c54d7da54f4a
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-01-18 20:04:39 +0800

    prepare

--- app/src/main/java/com/example/svctest/MainActivity.java
+++ app/src/main/java/com/example/svctest/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.svctest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button start;
+    private Button stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start = findViewById(R.id.btnStart);
+        stop = findViewById(R.id.btnStop);
+
+        start.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.i("TAG", "start.setOnClickListener called");
+            }
+        });
+
+        stop.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TAG", "stop.setOnClickListener called");
+            }
+        });
     }
 }
